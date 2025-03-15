1d94d994f2a2b10e159248c5cc1025ca987e9fa7
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-15 16:40:19 +0530

    completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Signup from './components/Signup'
 import Products from './components/Products'
 import { BrowserRouter } from 'react-router-dom'
 import AllRouting from './AllRouting'
+import Navbar from './components/Navbar'
 
 function App() {
   
@@ -12,10 +13,11 @@ function App() {
   return (
     <>
       <BrowserRouter>
+       <Navbar/>
        <AllRouting/>
       </BrowserRouter>
     </>
   )
-}
+}1
 
 export default App
