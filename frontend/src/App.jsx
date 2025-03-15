d0b5c0849ebec5f22d8de4320c18f369dc0691a2
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-15 12:14:14 +0530

    completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
 import { useState } from 'react'
- //import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
 import './App.css'
 import Login from './components/Login'
 import Signup from './components/Signup'
 import Products from './components/Products'
+import { BrowserRouter } from 'react-router-dom'
+import AllRouting from './AllRouting'
 
 function App() {
   
 
   return (
     <>
-      <Products/>
-      <Login/>
-      <Signup/>
+      <BrowserRouter>
+       <AllRouting/>
+      </BrowserRouter>
     </>
   )
 }
