d51f0c2a544a4c87705576b298cafee3acd5d153
KUSHALMANDALA07 <kushalmandala123@gmail.com>
2025-03-28 11:12:10 +0530

    completed

--- backend/index.js
+++ backend/index.js
@@ -39,11 +39,32 @@ app.get("/",(req,res)=>{
 
 app.use("/user",useRouter);
 
-app.use("/product",productRouter);
+app.use("/product",async (req, res, next) => {
+    try {
+        const token = req.header("Authorization");
+        console.log(token)
+        if (!token) {
+            return res.status(401).json({ message: "Please login" });
+        }
+        
+        const decoded = jwt.verify(token, process.env.JWT_PASSWORD);
+        const user = await userModel.findById(decoded.id);
+        
+        if (!user && user.id) {
+            return res.status(404).json({ message: "Please signup" });
+        }
+        console.log(user.id)
+        req.userId = user.id; 
+        next();
+    } catch (error) {
+        console.log(error)
+        return res.status(400).json({ message: "Invalid Token", error });
+    }
+},productRouter);
 
 app.listen(PORT,async ()=>{
     try {
-       await mongoose.connect(process.env.MONGO_URL);
+       await mongoose.connect(`mongodb+srv://abhishektiwari136136:${MONGO_PASSWORD}@cluster0.55lt4.mongodb.net/`);
        console.log("Connected sucessfully");
     } catch (error) {
         console.log("Something went wrong not able to connect to server",error);
